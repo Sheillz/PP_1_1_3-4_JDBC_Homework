339a88e04e7ccd98381b4f8188728aae3ac9f281
kata1 <black.smetana777@mail.ru>
2023-12-27 17:18:49 +0300

    /PP_1_1_3-4_JDBC 1.add id 2.add user

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -15,8 +15,8 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Statement statement = connection.createStatement()){
             statement.executeUpdate( "CREATE TABLE  IF NOT EXISTS user " +
-                    "(id serial,  name VARCHAR(100), " +
-                    "lastName VARCHAR(100), age  smallint,PRIMARY KEY(id))");
+                    "(id SERIAL,  name VARCHAR(100), " +
+                    "lastName VARCHAR(100), age  SMALLINT,PRIMARY KEY(id))");
         } catch (SQLException e) {
             System.out.println("An error occurred while creating the table" + e.getMessage());
             throw new RuntimeException(e);
