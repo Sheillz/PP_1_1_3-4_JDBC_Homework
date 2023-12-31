20aca980938c349bb763b1b40008a56a054f4325
kata1 <black.smetana777@mail.ru>
2023-12-31 17:53:49 +0300

    /PP_1_1_3-4_Hb

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,8 +8,6 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //UserDao dao = new UserDaoHibernateImpl();
-
     UserDao dao = new UserDaoJDBCImpl();
     @Override
     public void createUsersTable() {
